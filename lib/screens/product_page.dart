c0ed72a01d39ffda82b6495897f732c78a021a6c
Geeky-star <nimratkaur540@gmail.com>
2020-12-19 19:26:05 +0530

    Initial commit

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -5,7 +5,6 @@ import 'package:ecommerce_app/widgets/product_size.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/widgets/products_size.dart';
 
 class ProductPage extends StatefulWidget {
   final String productId;
